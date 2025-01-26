3f6fb0ab1249d8ae1acd63df4356b2889f859cea
cessio <hagann.niebaum@gmail.com>
2025-01-25 23:53:45 -0500

    Version 1.0
    This is an extremely barebones fabric mod that displays the number of total minecraft days in the top left of the players screen.
    
    To change the day, use /setday <dayNumber>
    
    Yes it's sort of broken and technically "adds" days instead of setting them
    will I fix it? yes
    
    Can you toggle it off? no
    
    Can non-ops use the command? yes.
    
    its kind of a piece of shit but its MY piece of shit and I plan to update it and maybe make it something I can feel proud of in future updates.
    
    Also I feel the need to come clean and admit that a lot of this code comes from chatGPT. GPT made a lot of weird bugs, which made me end up having to learn how to do everything myself anyway, so I think it all balances out.

--- src/main/java/net/cessio/displaydaycounter/DayCounterDisplay.java
+++ src/main/java/net/cessio/displaydaycounter/DayCounterDisplay.java
@@ -1,7 +1,12 @@
 package net.cessio.displaydaycounter;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +17,19 @@ public class DayCounterDisplay implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("setday")
+					.then(CommandManager.argument("day", IntegerArgumentType.integer(0))
+							.suggests(new DevCleanSuggestionProvider())
+							.executes(context -> {
+								int day = IntegerArgumentType.getInteger(context, "day");
+								ServerWorld world = context.getSource().getWorld();
+								world.setTimeOfDay(world.getTime() + day * 24000L);
+								context.getSource().sendFeedback(() -> Text.literal("Day set to: "+ day), true);
+								return 1;
+							})
+					)
+			);
+		});
 	}
 }
--- src/main/java/net/cessio/displaydaycounter/DayCounterDisplayClient.java
+++ src/main/java/net/cessio/displaydaycounter/DayCounterDisplayClient.java
@@ -1,10 +1,35 @@
 package net.cessio.displaydaycounter;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.text.Text;
 
+@Environment(EnvType.CLIENT)
 public class DayCounterDisplayClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Register a callback to render the HUD
+        HudRenderCallback.EVENT.register(this::renderDayInfo);
+    }
 
+    private void renderDayInfo(DrawContext context, RenderTickCounter tickDelta)
+    {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if(client.world != null)
+        {
+            //get the in-game time and calculate the day
+            long dayTime = client.world.getTimeOfDay();
+            long dayNumber = dayTime / 24000;
+
+            //Display the day number in the top-left corner
+            String dayText = "Day: " + dayNumber;
+            context.drawText(client.textRenderer, Text.literal(dayText), 10, 10, 0xFFFFFF, true);;
+        }
     }
 }
